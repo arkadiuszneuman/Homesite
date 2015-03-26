a96279621098564327750742799ef8448fdf8e79
Arkadiusz Neuman <mrmoro@o2.pl>
2015-03-26 23:06:25 +0100

    Init

---
+++ example.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use("/bower_components", express.static(__dirname + '/bower_components'));
+
+app.get('/', function (req, res) {
+  res.sendFile('html/index.html', {"root": __dirname});
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
