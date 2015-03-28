7127d42ed3d110b2c855ac278092fb9238182972
Arkadiusz Neuman <mrmoro@o2.pl>
2015-03-28 15:42:21 +0100

    Added parallax background

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use("/bower_components", express.static(__dirname + '/bower_components'));
-app.use("/js", express.static(__dirname + '/js'));
-app.use("/", express.static(__dirname + '/html'));
-app.use("/css", express.static(__dirname + '/css'));
+app.use("/", express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile('html/index.html', {"root": __dirname});
+  res.sendFile('public/html/index.html', {"root": __dirname});
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
