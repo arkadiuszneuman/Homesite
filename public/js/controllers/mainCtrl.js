9d9daafce344449c63a99236f1f2eba5dbcc5ea2
Arkadiusz Neuman <mrmoro@o2.pl>
2015-04-07 19:23:51 +0200

    Added technologies section

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,5 +1,15 @@
-app.controller('mainCtrl', ['$scope', function($scope){
+app.controller('mainCtrl', ['$scope', '$timeout', function($scope, $timeout){
 	$scope.year = new Date().getFullYear();
 
 	$scope.introTextShowed = true;
+
+	$(document).ready(function() {
+		$scope.headingAnim = "fadeInLeft";
+		$timeout(function() {
+			$scope.leadAnim = "fadeInRight show";
+			$timeout(function() {
+				$scope.buttonAnim = "fadeInUp";
+			}, 300);
+		}, 300);
+	});
 }]);
